2e446508b760eb6a304b8fde07e3fb08cdfb805c
bytelook <89795751+bytelook@users.noreply.github.com>
2021-11-15 16:21:21 +0800

    druidtwosource created!

---
+++ druidtwosource/src/main/java/byteinfo/top/druidtwosource/DruidtwosourceApplication.java
@@ -0,0 +1,13 @@
+package byteinfo.top.druidtwosource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DruidtwosourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DruidtwosourceApplication.class, args);
+    }
+
+}
---
+++ druidtwosource/src/test/java/byteinfo/top/druidtwosource/DruidtwosourceApplicationTests.java
@@ -0,0 +1,13 @@
+package byteinfo.top.druidtwosource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DruidtwosourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
