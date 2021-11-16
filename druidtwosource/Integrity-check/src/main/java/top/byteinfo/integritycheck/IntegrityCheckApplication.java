f9bd46d49b5870b4ec691e22e0f102cc2f51d636
bytelook <89795751+bytelook@users.noreply.github.com>
2021-11-16 13:59:26 +0800

    Integrity check
    add knife4j
    
    去掉springboot中和数据源相关的自动装配；

--- druidtwosource/Integrity-check/src/main/java/top/byteinfo/integritycheck/IntegrityCheckApplication.java
+++ druidtwosource/Integrity-check/src/main/java/top/byteinfo/integritycheck/IntegrityCheckApplication.java
@@ -2,8 +2,12 @@ package top.byteinfo.integritycheck;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude ={
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,})
 public class IntegrityCheckApplication {
 
     public static void main(String[] args) {
