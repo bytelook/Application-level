8cac8248ab185aba5f07b014aecfcb9669c4dab9
bytelook <89795751+bytelook@users.noreply.github.com>
2021-11-16 13:51:30 +0800

    Integrity check

---
+++ druidtwosource/Integrity-check/src/main/java/top/byteinfo/integritycheck/IntegrityCheckApplication.java
@@ -0,0 +1,13 @@
+package top.byteinfo.integritycheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrityCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegrityCheckApplication.class, args);
+    }
+
+}
---
+++ druidtwosource/Integrity-check/src/test/java/top/byteinfo/integritycheck/IntegrityCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package top.byteinfo.integritycheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrityCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
