ccbf7470cd446ea52bb0cdc49eaaae1b81970069
bytelook <89795751+bytelook@users.noreply.github.com>
2021-11-16 14:27:22 +0800

    add iml

--- druidtwosource/Integrity-check/src/main/java/top/byteinfo/integritycheck/config/Knife4jConfiguration.java
+++ druidtwosource/Integrity-check/src/main/java/top/byteinfo/integritycheck/config/Knife4jConfiguration.java
@@ -29,7 +29,6 @@ public class Knife4jConfiguration {
                         .version("1.0")
                         .build())
                 //分组名称
-
                 .groupName("2.X版本")
                 .select()
                 //这里指定Controller扫描包路径
